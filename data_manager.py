1dd3b15253ff16c5c7af8da5cc9f417db1a0f903
magg666 <mw23127@gmail.com>
2018-12-04 18:36:28 +0100

    added file to read and write files as csv

---
+++ data_manager.py
@@ -0,0 +1,19 @@
+file_name = 'crm/clients.csv'
+
+
+def make_table_from_data(file_name):
+    """
+    Takes data from table end returns as list of lists
+    """
+
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+        table = [element.replace('\n', '').split(',') for element in lines]
+    return table
+
+
+def write_data_in_document(file_name, table):
+    with open(file_name, "w") as file:
+        for line in table:
+            row = ','.join(line)
+            file.write(row + "\n")
