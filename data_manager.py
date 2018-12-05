0c5a44994e6ce783f6fa64cd1282d2a662e1b859
magg666 <mw23127@gmail.com>
2018-12-05 18:37:43 +0100

    modifications

--- data_manager.py
+++ data_manager.py
@@ -5,11 +5,13 @@ def make_table_from_data(file_name):
     """
     Takes data from table end returns as list of lists
     """
-
-    with open(file_name, 'r') as file:
-        lines = file.readlines()
-        table = [element.replace('\n', '').split(',') for element in lines]
-    return table
+    try:
+        with open(file_name, 'r') as file:
+            lines = file.readlines()
+            table = [element.replace('\n', '').split(',') for element in lines]
+        return table
+    except FileNotFoundError:
+        return 'Error - there is not such file.'
 
 
 def write_data_in_document(file_name, table):
--- ui.py
+++ ui.py
@@ -10,3 +10,29 @@ def make_columns_for(table_headers, table):
 
     columns = [list(item) for item in zip(table_headers, (*table))]
     return columns
+
+
+# counts length of every item in table
+def count_every_item_length():
+    """
+    Counts length of every item in table converted into columns
+    No Arg
+    Returns: Length (as number) packed in list of lists
+    """
+
+    columns = make_columns_for(table_headers, table)
+    every_item_length = [[len(elems)for elems in line]for line in columns]
+    return every_item_length
+
+
+# select highest length of items in every colum in table
+def find_max_length_item():
+    """
+    Takes all items' length and finds longest - in each column
+    No Args
+    Returns: List of numbers
+    """
+
+    every_item_length = count_every_item_length()
+    max_item_length = [max(line) for line in every_item_length]
+    return max_item_length
